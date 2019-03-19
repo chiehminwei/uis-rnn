ccfe321434600a9c793e0108a10218cf79f920d4
Chiehmin Wei <cmw025@bucknell.edu>
2019-03-19 19:29:05 +0000

    d

--- demo.py
+++ demo.py
@@ -70,7 +70,9 @@ def diarization_experiment(model_args, training_args, inference_args):
   for i in range(train_sequences.shape[0]):
     train_sequence = train_sequences[i]
     trian_cluster_id = train_cluster_ids[i]
+    print(trian_cluster_id)
     trian_cluster_id = trian_cluster_id.tolist()
+    print(trian_cluster_id)
 
     d = vars(training_args)
     d['learning_rate'] = 1e-3
