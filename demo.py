d7ceec5a6fbe27b81c49e4a20c7652443eccc9ca
Chiehmin Wei <cmw025@bucknell.edu>
2019-03-19 20:42:22 +0000

    d

--- demo.py
+++ demo.py
@@ -54,7 +54,7 @@ def diarization_experiment(model_args, training_args, inference_args):
 
 
 
-  chunk_size = train_sequences.shape[0] // 1000
+  chunk_size = train_sequences.shape[0] // 10000
   left_over = train_sequences.shape[0] % chunk_size
   new_len = train_sequences.shape[0] - left_over
 
