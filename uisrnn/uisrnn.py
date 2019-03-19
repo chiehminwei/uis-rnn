56cfcd6e001953325f3ace588fed8b5e9548db1f
Chiehmin Wei <cmw025@bucknell.edu>
2019-03-19 19:01:03 +0000

    d

--- uisrnn/uisrnn.py
+++ uisrnn/uisrnn.py
@@ -224,6 +224,9 @@ class UISRNN:
       raise ValueError('train_sequence does not match the dimension specified '
                        'by args.observation_dim.')
     if train_total_length != len(train_cluster_id):
+      print(train_total_length)
+      print(observation_dim)
+      print(len(train_cluster_id))
       raise ValueError('train_sequence length is not equal to '
                        'train_cluster_id length.')
 
