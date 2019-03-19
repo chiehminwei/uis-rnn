15997d8e3d6ab02f6d9a2e1e40e4e4cc6b7a1130
Chiehmin Wei <cmw025@bucknell.edu>
2019-03-19 18:28:44 +0000

    hope

--- demo.py
+++ demo.py
@@ -66,9 +66,8 @@ def diarization_experiment(model_args, training_args, inference_args):
 
   # training
   for train_sequence, trian_cluster_id in zip(train_sequences, train_cluster_ids):
-    print(training_args)
-    print(type(training_args))
-    training_args['learning_rate'] = 1e-3
+    d = vars(training_args)
+    d['learning_rate'] = 1e-3
     model.fit(train_sequence, train_cluster_id, training_args)
   
   model.save(SAVED_MODEL_NAME)
