11859eb09bf7863ab3062b414c4036676cf00ffb
Chiehmin Wei <cmw025@bucknell.edu>
2019-03-20 11:52:56 +0000

    defaq

--- demo_test.py
+++ demo_test.py
@@ -45,7 +45,10 @@ def diarization_experiment(model_args, training_args, inference_args):
   orig_test_sequences = np.load('data/test_sequence.npy').astype(np.float64)
   orig_test_cluster_ids = np.array(np.load('data/test_cluster_id.npy'))
 
-  test_chunk_size = orig_test_sequences.shape[0] // 1000
+  orig_test_sequences = orig_test_sequences[:orig_test_sequences.shape[0]//100]
+  orig_test_cluster_ids = orig_test_cluster_ids[:orig_test_sequences.shape[0]//100]
+
+  test_chunk_size = orig_test_sequences.shape[0] // 86
   test_left_over = orig_test_sequences.shape[0] % test_chunk_size
   test_new_len = orig_test_sequences.shape[0] - test_left_over
 
